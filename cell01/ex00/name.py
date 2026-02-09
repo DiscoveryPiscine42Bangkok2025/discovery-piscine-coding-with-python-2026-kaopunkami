8fbc16b04eb192b8b09c1e4b3cd2ea5d1465fe08
kaopunkami <66050504@kmitl.ac.th>
2026-02-09 13:40:07 +0700

    First commit

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Rathanon"
+last_name = "Nilsalub"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Rathanon"
+last_name = "Nilsalub"
+whole_name = first_name + " " + last_name
+print(whole_name)
